c668d68256ef7b965bee5db0f3a2f7325aa9971b
PVignos <pietro.vignola@gmail.com>
2023-05-03 00:50:20 +0200

    feat: using codegen to autogen types

---
+++ codegen.ts
@@ -0,0 +1,15 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  schema: 'https://rickandmortyapi.com/graphql',
+  ignoreNoDocuments: true,
+  generates: {
+    './src/gql/': {
+      documents: ['src/**/*.tsx'],
+      preset: 'client',
+      plugins: [],
+    },
+  },
+}
+
+export default config
