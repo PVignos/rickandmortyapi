f1f0b80d5c98bb0d92d2b9003f9144917e06fa14
PVignos <pietro.vignola@gmail.com>
2023-05-03 22:40:47 +0200

    feat: adding info

--- src/queries/rickandmortyapi.ts
+++ src/queries/rickandmortyapi.ts
@@ -3,6 +3,12 @@ import {gql} from "@apollo/client";
 const GET_ALL_CHARACTERS = gql`
     query Character($page: Int) {
         characters(page: $page) {
+            info {
+                count
+                next
+                prev
+                pages
+            }
             results {
                 id
                 name
